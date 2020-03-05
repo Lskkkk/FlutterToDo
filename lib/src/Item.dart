77cf1a22e3632c97f5c78250af2fd07c922132e0
skliu <skliu@ctrip.com>
2020-03-05 17:44:04 +0800

    feat: use left slide to toggle delete button

--- lib/src/Item.dart
+++ lib/src/Item.dart
@@ -20,41 +20,92 @@ class _Item extends State<Item> {
   final Function delete;
   _Item(this.content, this.isSelected, this.toggle, this.delete);
 
+  static double deleteButtonWidth = 90;
   bool onTouch = false;
+  double distance = deleteButtonWidth;
 
   @override
   Widget build(BuildContext context) {
     return GestureDetector(
-      onLongPressStart: (LongPressStartDetails details) {
-        setState(() {
-          onTouch = true;
-        });
+      onHorizontalDragUpdate: (DragUpdateDetails details) {
+        final dx = details.delta.dx;
+        if (dx < 0) {
+          onScrollingLeft(dx);
+        } else {
+          onScrollingRight(dx);
+        }
       },
-      onLongPressEnd: (LongPressEndDetails details) {
-        setState(() {
-          onTouch = false;
-        });
-        this.delete(this.content);
+      onHorizontalDragEnd: (DragEndDetails details) {
+        onScrollEnd();
       },
       child: Container(
           color: this.onTouch
               ? Color.fromRGBO(123, 12, 32, 0.5)
               : Color.fromRGBO(235, 235, 235, 0.5),
           margin: EdgeInsets.fromLTRB(0, 0, 0, 2),
-          padding: EdgeInsets.fromLTRB(5, 8, 0, 8),
           child: Row(
             children: <Widget>[
-              Checkbox(
-                  onChanged: (val) {
-                    this.toggle(this.content);
-                  },
-                  value: this.isSelected),
-              Text(
-                this.content,
-                style: TextStyle(fontSize: 20, color: Colors.black87),
-              ),
+              Expanded(
+                  child: Row(children: <Widget>[
+                Checkbox(
+                    onChanged: (val) {
+                      this.toggle(this.content);
+                    },
+                    value: this.isSelected),
+                Text(
+                  this.content,
+                  style: TextStyle(fontSize: 20, color: Colors.black87),
+                ),
+              ])),
+              Transform.translate(
+                offset: Offset(distance, 0),
+                child: Container(
+                  width: deleteButtonWidth,
+                  height: 60,
+                  color: Color.fromRGBO(255, 50, 50, 0.4),
+                  child: IconButton(
+                      icon: Icon(Icons.delete_outline),
+                      onPressed: () {
+                        this.delete(this.content);
+                      }),
+                ),
+              )
             ],
           )),
     );
+  }
+
+  onScrollingLeft(dx) {
+    if (distance == 0) return;
+    if (distance < 0) {
+      setState(() {
+        distance = 0;
+      });
+    } else {
+      setState(() {
+        distance += dx;
+      });
+    }
+  }
+
+  onScrollEnd() {
+    if (distance != 0) {
+      setState(() {
+        distance = deleteButtonWidth;
+      });
+    }
+  }
+
+  onScrollingRight(dx) {
+    if (distance == deleteButtonWidth) return;
+    if (distance > deleteButtonWidth - 10) {
+      setState(() {
+        distance = deleteButtonWidth;
+      });
+    } else {
+      setState(() {
+        distance += dx;
+      });
+    }
   }
 }
